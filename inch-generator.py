813255771331d1db242a2856f16e52f72a79bb37
Nathan Miller <nathan@nathanmiller.wtf>
2022-08-11 01:28:48 -0500

    Cleaned up sub-generators

--- inch-generator.py
+++ inch-generator.py
@@ -1,31 +1,36 @@
+#!/usr/bin/env python3
+
 from fractions import Fraction
-import fractions
 
-inch_scaler = (1000.0 / 16)
+def generate_inch_lines():
+    output = ""
+    inch_scaler = (1000.0 / 16)
 
-fmt = """            <g>
-                <line
-                    style="stroke: rgb(0,0,0); stroke-width: 2;"
-                    vector-effect="non-scaling-stroke"
-                    x1="-100"
-                    y1="{0:.1f}"
-                    x2="0"
-                    y2="{0:.1f}" />
-                <text
-                    x="-110"
-                    y="{0:.1f}"
-                    font-size="2em"
-                    font-family="sans-serif"
-                    font-weight="bold"
-                    text-anchor="end"
-                    alignment-baseline="middle">{1}/{2}"</text>
-            </g>"""
+    fmt = """
+                <g>
+                    <line
+                        style="stroke: rgb(0,0,0); stroke-width: 2;"
+                        vector-effect="non-scaling-stroke"
+                        x1="-100"
+                        y1="-{0:.01f}"
+                        x2="0"
+                        y2="-{0:.01f}" />
+                    <text
+                        x="-110"
+                        y="-{0:.01f}"
+                        font-size="2em"
+                        font-family="sans-serif"
+                        font-weight="bold"
+                        text-anchor="end"
+                        alignment-baseline="middle">{1}/{2}"</text>
+                </g>"""
 
-print('        <g id="inches">')
+    for i in range(15,1,-1):
+        fractional_inch = Fraction(i, 16)
+        y_val = round(inch_scaler * i, 2)
+        output += fmt.format(y_val, fractional_inch.numerator, fractional_inch.denominator)
 
-for i in range(15,0,-1):
-    y_val = round(inch_scaler * i, 2)
-    fractional_inch = Fraction(i, 16)
-    print(fmt.format(y_val * -1, fractional_inch.numerator, fractional_inch.denominator))
+    return output
 
-print("</g>")
+if __name__ == "__main__":
+    print(generate_inch_lines())
--- mm-generator.py
+++ mm-generator.py
@@ -1,24 +1,34 @@
-mm_scaler = (1000.0 / 25.4)
+#!/usr/bin/env python3
 
-fmt = """
-            <g>
-                <line
-                    style="stroke: rgb(0,0,0); stroke-width: 2;"
-                    vector-effect="non-scaling-stroke"
-                    x1="100"
-                    y1="{0:.02f}"
-                    x2="0"
-                    y2="{0:.02f}" />
-                <text
-                    x="110"
-                    y="{0:.02f}"
-                    font-size="2em"
-                    font-family="sans-serif"
-                    font-weight="bold"
-                    text-anchor="start"
-                    alignment-baseline="middle">{1} mm</text>
-            </g>"""
+def generate_mm_lines():
+    output = ""
 
-for i in range(25,0,-1):
-    y_val = round(mm_scaler * i, 2)
-    print(fmt.format(y_val * -1, i))
+    mm_scaler = (1000.0 / 25.4)
+
+    fmt = """
+                <g>
+                    <line
+                        style="stroke: rgb(0,0,0); stroke-width: 2;"
+                        vector-effect="non-scaling-stroke"
+                        x1="100"
+                        y1="-{0:.02f}"
+                        x2="0"
+                        y2="-{0:.02f}" />
+                    <text
+                        x="110"
+                        y="-{0:.02f}"
+                        font-size="2em"
+                        font-family="sans-serif"
+                        font-weight="bold"
+                        text-anchor="start"
+                        alignment-baseline="middle">{1} mm</text>
+                </g>"""
+
+    for i in range(24,1,-1):
+        y_val = round(mm_scaler * i, 2)
+        output += fmt.format(y_val, i)
+
+    return output
+
+if __name__ == "__main__":
+    print(generate_mm_lines())
