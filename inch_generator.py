e6e08c84d54cd65e517d7414d109ceca4da01056
Nathan Miller <nathan@nathanmiller.wtf>
2022-08-21 01:58:18 -0500

    Added formatting, 1/32s of an inch

--- inch_generator.py
+++ inch_generator.py
@@ -1,34 +1,58 @@
 #!/usr/bin/env python3
 
+from cgitb import text
 from fractions import Fraction
+
+step_size = 32
+
+text_offset = 10
+inch_scaler = (1000.0 / step_size)
 
 def generate_inch_lines():
     output = ""
-    inch_scaler = (1000.0 / 16)
 
     fmt = """
-                <g>
+                <g opacity="{6:.02f}">
                     <line
                         style="stroke: rgb(0,0,0); stroke-width: 2;"
                         vector-effect="non-scaling-stroke"
-                        x1="100"
+                        x1="{3}"
                         y1="-{0:.01f}"
                         x2="0"
                         y2="-{0:.01f}" />
                     <text
-                        x="110"
+                        x="{4}"
                         y="-{0:.01f}"
-                        font-size="2em"
+                        font-size="{5:.01f}em"
                         font-family="sans-serif"
                         font-weight="bold"
                         text-anchor="start"
                         alignment-baseline="middle">{1}/{2}"</text>
                 </g>"""
 
-    for i in range(15,0,-1):
-        fractional_inch = Fraction(i, 16)
+    for i in range((step_size-1),0,-1):
+        opacity = 1
+        line_width = 150
+        font_size_em = 2
+        fractional_inch = Fraction(i, step_size)
+
+        if fractional_inch.denominator == 32:
+            opacity = 0.5
+            font_size_em = 1.5
+            line_width = 50
+
+        if fractional_inch.denominator == 16:
+            font_size_em = 1.7
+            line_width = 100
+
         y_val = round(inch_scaler * i, 2)
-        output += fmt.format(y_val, fractional_inch.numerator, fractional_inch.denominator)
+        output += fmt.format(y_val,
+            fractional_inch.numerator,
+            fractional_inch.denominator,
+            line_width,
+            line_width + text_offset,
+            font_size_em,
+            opacity)
 
     return output
 
--- mm_generator.py
+++ mm_generator.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
 mm_scaler = (1000.0 / 25.4)
+values = [ *range(25,0,-1), *[ 5.5 ] ]
 important_values = [ 8, 10, 12, 14, 15, 17, 19, 21 ]
+quiet_values = [ 1, 2, 3, 5.5, 25 ]
 text_offset = 10
 
 def generate_mm_lines():
     output = ""
 
     fmt = """
-                <g>
+                <g opacity="{5:.02f}">
                     <line
                         style="stroke: rgb(0,0,0); stroke-width: 2;"
                         vector-effect="non-scaling-stroke"
@@ -19,19 +21,34 @@ def generate_mm_lines():
                     <text
                         x="-{3}"
                         y="-{0:.02f}"
-                        font-size="2em"
+                        font-size="{4:.01f}em"
                         font-family="sans-serif"
                         font-weight="bold"
                         text-anchor="end"
                         alignment-baseline="middle">{1} mm</text>
                 </g>"""
 
-    for i in range(24,0,-1):
-        line_width = 100
+    for i in values:
+        opacity = 1
+        font_size_em = 1.7
+        line_width = 75
+
         if i in important_values:
-            line_width = 200
+            line_width = 150
+            font_size_em = 2
+
+        if i in quiet_values:
+            opacity=0.5
+            line_width = 25
+            font_size_em = 1.3
+
         y_val = round(mm_scaler * i, 2)
-        output += fmt.format(y_val, i, line_width, line_width + text_offset)
+        output += fmt.format(y_val,
+            i,
+            line_width,
+            line_width + text_offset,
+            font_size_em,
+            opacity)
 
     return output
 
